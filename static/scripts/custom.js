e6b160920cb304b67d8f2af20da1d78a0ef56a6d
Ben Stein-Lobovits <ben@ripple.com>
2016-05-23 13:04:27 -0700

    dark theme mode

--- static/scripts/custom.js
+++ static/scripts/custom.js
@@ -1,11 +1,30 @@
 (function($){
 
-  //on-off switch
+  //Dark theme
   $(function() {
+
+    var darkTheme = false;
+
     $(".onoffswitch-label").click(function() {
-      $('body').toggleClass('dark');
-    });
+
+      darkTheme = !darkTheme;
+
+      if(darkTheme) {
+        $('body').toggleClass('dark');
+        localStorage.setItem('theme', 'dark');
+       } else {
+        $('body').toggleClass('dark');
+        localStorage.removeItem('theme');
+       }
+     });
+
+    if (localStorage['theme'] == "dark") {
+      $('.onoffswitch-label')[0].click();
+      $('body').addClass('dark');
+    }
+
   });
+
 
   // Bootstrap Scrollspy
   // var $scrollSpy = $(this).scrollspy({ target: '#scrollingNav', offset: 18 });
